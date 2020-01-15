1e0cbcc5f4ca2676fbbebf20cb4d587ca40f36a1
Viktor Andersen <vikfand@gmail.com>
2020-01-15 23:36:43 +0100

    Update deploy scripts.

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -8,5 +8,7 @@ docker push vikfand/strapi-dev:latest
 
 ssh root@140.82.37.146 << EOF
 cd strapi
+git fetch
+git reset --hard origin/master
 ./update-dev.sh
 EOF
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -8,5 +8,7 @@ docker push vikfand/strapi:latest
 
 ssh root@140.82.37.146 << EOF
 cd strapi
+git fetch
+git reset --hard origin/master
 ./update-prod.sh
 EOF
