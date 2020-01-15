5afc15d3f75c1c999a67fba8364d0674e5dc0225
Viktor Andersen <vikfand@gmail.com>
2020-01-15 16:14:21 +0100

    Update update scripts

--- update-dev.sh
+++ update-dev.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-git fetch
-git reset --hard origin/master
 docker pull vikfand/strapi-dev:latest
 docker-compose up -d --no-deps api-dev
--- update-prod.sh
+++ update-prod.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-git fetch
-git reset --hard origin/master
 docker pull vikfand/strapi:latest
 docker-compose up -d --no-deps api-prod
