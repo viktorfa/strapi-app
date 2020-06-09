540af8bcb800254f30adbcf4bc86d2f1e1fee7f3
Viktor Andersen <vikfand@gmail.com>
2020-06-09 05:56:42 +0200

    Update to Strapi 3.0.1 stable

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -6,6 +6,8 @@
  *
  * This gives you an opportunity to set up your data model,
  * run jobs, or perform some special logic.
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
 module.exports = () => {};
